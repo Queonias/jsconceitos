f4d2131fe09596e042d7acefefdad935c6f3a0a6
queones <queones@gmail.com>
2025-03-29 20:41:12 -0300

    Service / Provider - entendendo o básico da injeção de dependências do NestJS

--- src/conceitos-manual/conceitos-manual.module.ts
+++ src/conceitos-manual/conceitos-manual.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConceitosManualController } from './conceitos-manual.controller';
+import { ConceitosManualService } from './conceitos-manual.service';
 
 @Module({
   imports: [],
   controllers: [ConceitosManualController],
-  providers: [],
+  providers: [ConceitosManualService],
 })
 export class ConceitosManualModule {}
