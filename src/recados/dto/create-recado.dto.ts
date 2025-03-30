da48ed79b588837c4d478161edecc02bc7dae90a
queones <queones@gmail.com>
2025-03-30 12:55:50 -0300

    Use PartialType de Mapped-Types para a validação de campos em UpdateRecadoDto

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -6,11 +6,13 @@ export class CreateRecadoDto {
   @MinLength(5)
   @MaxLength(255)
   readonly texto: string;
+
   @IsString()
   @IsNotEmpty()
   @MinLength(2)
   @MaxLength(50)
   readonly de: string;
+
   @IsString()
   @IsNotEmpty()
   @MinLength(2)
