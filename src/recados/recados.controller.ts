4003160df3fb7b1c89bf68864a834f9b34503f22
queones <queones@gmail.com>
2025-03-29 21:02:52 -0300

    Criando parâmetros de rota de forma dinâmica para o ID do recurso - @Get(':id')

---
+++ src/recados/recados.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('recados')
+export class RecadosController {
+  @Get()
+  findAll() {
+    return 'Essa rota retorna todos os recados';
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'Essa rota retorna um recado específico';
+  }
+}
