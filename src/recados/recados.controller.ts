cc6181c60e97eee890b18a3c80db66180192fc5b
queones <queones@gmail.com>
2025-04-05 16:08:16 -0300

    Interceptors - alterando os dados de resposta com map

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -22,6 +22,7 @@ import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interce
 import { TimingConnectionInterceptor } from 'src/common/interceptors/timing-connection.interceptor';
 import { ErrorHandlerInterceptor } from 'src/common/interceptors/error-handling.interceptor';
 import { SimpleCacheInterceptor } from 'src/common/interceptors/simple-cache.interceptor';
+import { ChangeDataInterceptor } from 'src/common/interceptors/change-data.interceptor';
 
 // CRUD - Create, Read, Update, Delete
 // Create - POST -> Criar
@@ -37,7 +38,7 @@ import { SimpleCacheInterceptor } from 'src/common/interceptors/simple-cache.int
 // DTO - Objeto simples -> validar dados / Transformar dados
 
 @Controller('recados')
-// @UseInterceptors(SimpleCacheInterceptor)
+// @UseInterceptors(ChangeDataInterceptor)
 export class RecadosController {
   constructor(private readonly recadosService: RecadosService) {}
   @HttpCode(HttpStatus.OK)
