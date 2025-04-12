0699ba3d973eaf8ff27c3461dc3ff993b97ec203
queones <queones@gmail.com>
2025-04-12 18:26:48 -0300

    Usando @hapi/joi para validar as configurações do .env (variáveis de ambiente)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
-import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
 import { RecadosModule } from 'src/recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
 import { MyExceptionFilter } from 'src/common/filters/my-exception.filter';
 import { SimpleMiddleware } from 'src/common/middlewares/simple.middleware';
 import { OutroMiddleware } from 'src/common/middlewares/outro.middleware';
-import { IsAdminGuard } from 'src/common/guards/is-admin.guard';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
@@ -18,6 +16,16 @@ import { ConfigModule } from '@nestjs/config';
       // envFilePath: '.env',
       // envFilePath: ['env/.env'],
       // ignoreEnvFile: true
+      validationSchema: Joi.object({
+        DATABASE_TYPE: Joi.required(),
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(3306),
+        DATABASE_USERNAME: Joi.required(),
+        DATABASE_DATABASE: Joi.required(),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_AUTOLOADENTITIES: Joi.number().min(0).max(1).default(0),
+        DATABASE_SYNCHRONIZE: Joi.number().min(0).max(1).default(0),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: process.env.DATABASE_TYPE as 'mssql',
