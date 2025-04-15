0ed65f29d20400187f53f17e71c844897c9a20d1
queones <queones@gmail.com>
2025-04-15 18:15:34 -0300

    Autenticando o usuário com email e senha usando o Hash de senha e HashingService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from './hashing/hashing.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+    private readonly hashingService: HashingService,
+  ) {}
   async login(loginDto: LoginDto) {
-    return loginDto;
+    let passwordIsValid = false;
+    let throwError = true;
+
+    const pessoa = await this.pessoaRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    if (pessoa) {
+      passwordIsValid = await this.hashingService.compare(loginDto.password, pessoa.passwordHash);
+    }
+
+    if (passwordIsValid) {
+      throwError = false;
+    }
+
+    if (throwError) {
+      throw new UnauthorizedException('Usuário ou senha inválidos');
+    }
+
+    return {
+      message: 'Usuário logado',
+    };
   }
 }
